55b4aff9a444f84e95870e09e2950eaa15726028
Moizuddin Siddiqui <moizuddinsiddiqui26@gmail.com>
2026-04-25 16:30:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,6 +45,7 @@ AUTO_REFRESH_INTERVAL = int(os.getenv("AUTO_REFRESH_INTERVAL", 30))  # seconds
 TOP_10_COINS = {
     "BTC": "bitcoin",
     "ETH": "ethereum",
+    "USDT": "tether",
     "BNB": "binancecoin",
     "XRP": "ripple",
     "SOL": "solana",
